66886c0729ec6488321c4bae0b4a4131e55737be
ugr0 <yujiro.takahashi0912@gmail.com>
2025-10-24 20:58:13 +0900

    feat: gcp setting

--- src/src/lib/logger.ts
+++ src/src/lib/logger.ts
@@ -1,14 +1,21 @@
+import { createGcpLoggingPinoConfig } from "@google-cloud/pino-logging-gcp-config";
 import pino from "pino";
 
-const isDev = process.env.NODE_ENV !== "production";
+const isLocal = process.env.NODE_ENV !== "production";
+
+const transport = isLocal
+  ? pino.transport({
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "yyyy-mm-dd HH:MM:ss.l o",
+        ignore: "pid,hostname",
+      },
+    })
+  : undefined;
 
 export const logger = pino(
-  isDev
-    ? pino.transport({
-        target: "pino-pretty",
-        options: { colorize: true, singleLine: false },
-      })
-    : undefined,
+  createGcpLoggingPinoConfig() as pino.LoggerOptions,
+  transport,
 );
 
-export default logger;
